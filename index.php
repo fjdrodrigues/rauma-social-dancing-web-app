34e48b013e95a36ddf71a63cb488bd9e9af1f400
Fábio Rodrigues <fjdrodrigues@hotmail.com>
2021-09-28 12:36:52 +0300

    alpha0.1#9 - Index: Meta properties for page sharing; Classes: Added Levels for classes; Home: creation of structure to faacilitate changes to carousel; Header and Leftpane: Added Note structure.

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
   <title>Rauma Social Dancing</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:image" content="https://i.ibb.co/Rp1YXn8/collage-1-900x425.png"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:url" content="http://www.raumasocialdancing.fi"/>
+  <meta property="og:title" content="Rauma Social Dancing"/>
+  <meta property="og:description" content="Rauma based social dancing promoters with fun and accessible classes."/>
   <link rel="icon" type="image/svg+xml"
 	href="https://gist.githubusercontent.com/fjdrodrigues/d41d59729cbbcc4506d1371384c2b2be/raw/c924dafcff99958828bbcc1515b68846d9b7fd08/angolan_r.svg">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,57 +8,88 @@ import { CarouselItem } from '../components/shared/carousel-item/carousel-item.i
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  public verticalCarouselItems: Array<CarouselItem> = [
+
+	private itemsContent = [
 		{
-			url: "https://i.ibb.co/tBvbpcV/collage-1-800x1000.png",
-			title: "New Season",
-			description: "Salsa, Bachata and Kizomba classes start in September",
+			verticalURL: "https://i.ibb.co/tBvbpcV/collage-1-800x1000.png",
+			horizontalURL: "https://i.ibb.co/Rp1YXn8/collage-1-900x425.png",
+			title: "Update for October",
+			description: "Kizomba classes on Mondays",
       hyperlink: environment.baseUrl+"/classes"
 		},
 		{
-			url: "https://i.ibb.co/Dtg04P7/collage-2-800x1000.png",
+			verticalURL: "https://i.ibb.co/Dtg04P7/collage-2-800x1000.png",
+			horizontalURL: "https://i.ibb.co/fDxJNhC/collage-2-900x425.png",
 			title: "Beginner Classes",
-			description: "Salsa, Bachata and Kizomba classes on Tuesdays",
+			description: "Solo Salsa, Couple Salsa and Couple Bachata classes on Tuesdays",
       hyperlink: environment.baseUrl+"/classes"
 		},
 		{
-			url: "https://i.ibb.co/mbKcM8G/collage-3-800x1000.png",
+			verticalURL: "https://i.ibb.co/mbKcM8G/collage-3-800x1000.png",
+			horizontalURL: "https://i.ibb.co/F6D1Vs4/collage-3-900x425.png",
 			title: "Intermediate Classes",
 			description: "Salsa and Bachata classes on Wednesdays",
       hyperlink: environment.baseUrl+"/classes"
 		},
 		{
-			url: "https://i.ibb.co/sm6jqht/social-2021-06-17-960x1200.png",
+			verticalURL: "https://i.ibb.co/sm6jqht/social-2021-06-17-960x1200.png",
+			horizontalURL: "https://i.ibb.co/R2CpHW2/social-2021-06-17-900x425.png",
 			title: "Socials",
 			description: "Every Friday and Sunday",
       hyperlink: environment.baseUrl+"/classes"
 		}
 	];
 
+  public verticalCarouselItems: Array<CarouselItem> = [
+		{
+			url: this.itemsContent[0].verticalURL,
+			title: this.itemsContent[0].title,
+			description: this.itemsContent[0].description,
+      hyperlink: this.itemsContent[0].hyperlink
+		},
+		{url: this.itemsContent[1].verticalURL,
+			title: this.itemsContent[1].title,
+			description: this.itemsContent[1].description,
+      hyperlink: this.itemsContent[1].hyperlink
+		},
+		{
+			url: this.itemsContent[2].verticalURL,
+			title: this.itemsContent[2].title,
+			description: this.itemsContent[2].description,
+      hyperlink: this.itemsContent[2].hyperlink
+		},
+		{
+			url: this.itemsContent[3].verticalURL,
+			title: this.itemsContent[3].title,
+			description: this.itemsContent[3].description,
+      hyperlink: this.itemsContent[3].hyperlink
+		}
+	];
+
 	public horizontalCarouselItems: Array<CarouselItem> = [
 		{
-			url: "https://i.ibb.co/Rp1YXn8/collage-1-900x425.png",
-			title: "New Season",
-			description: "Salsa, Bachata and Kizomba classes start in September",
-      hyperlink: environment.baseUrl+"/classes"
+			url: this.itemsContent[0].horizontalURL,
+			title: this.itemsContent[0].title,
+			description: this.itemsContent[0].description,
+      hyperlink: this.itemsContent[0].hyperlink
 		},
 		{
-			url: "https://i.ibb.co/fDxJNhC/collage-2-900x425.png",
-			title: "Beginner Classes",
-			description: "Salsa, Bachata and Kizomba classes on Tuesdays",
-      hyperlink: environment.baseUrl+"/classes"
+			url: this.itemsContent[1].horizontalURL,
+			title: this.itemsContent[1].title,
+			description: this.itemsContent[1].description,
+      hyperlink: this.itemsContent[1].hyperlink
 		},
 		{
-			url: "https://i.ibb.co/F6D1Vs4/collage-3-900x425.png",
-			title: "Intermediate Classes",
-			description: "Salsa and Bachata classes on Wednesdays",
-      hyperlink: environment.baseUrl+"/classes"
+			url: this.itemsContent[2].horizontalURL,
+			title: this.itemsContent[2].title,
+			description: this.itemsContent[2].description,
+      hyperlink: this.itemsContent[2].hyperlink
 		},
 		{
-			url: "https://i.ibb.co/R2CpHW2/social-2021-06-17-900x425.png",
-			title: "Socials",
-			description: "Every Friday and Sunday",
-      hyperlink: environment.baseUrl+"/classes"
+			url: this.itemsContent[3].horizontalURL,
+			title: this.itemsContent[3].title,
+			description: this.itemsContent[3].description,
+      hyperlink: this.itemsContent[3].hyperlink
 		}
 	];
 }
